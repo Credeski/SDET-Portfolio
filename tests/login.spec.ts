543d5ff79d035ef4842e5365223b2497808595a1
Solomon Chukwu <theonlycredeski@gmail.com>
2025-10-25 17:15:40 +0100

    Add dotenv dependency and implement login tests with Playwright

---
+++ tests/login.spec.ts
@@ -0,0 +1,71 @@
+import { test, expect, chromium } from '@playwright/test';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+
+
+/* test.beforeEach(async ({ page }) => {
+  await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login', {
+    waitUntil: 'domcontentloaded',
+    timeout: 60000
+  });
+}); */
+
+const username = process.env.ORANGE_HRM_USERNAME;
+if (!username) throw new Error('USERNAME not set');
+const password = process.env.ORANGE_HRM_PASSWORD;
+if (!password) throw new Error('PASSWORD not set');
+// Positive login test
+test('login test', async ({ page }) => {
+  await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
+  // Fill in username
+  await page.locator('input[name="username"]').fill(username);
+
+  // Fill in password
+  await page.locator('input[name="password"]').fill(password);
+
+  // Click login button
+  await page.locator('button[type="submit"]').click();
+
+  // Expect to be logged in by checking for the presence of the dashboard
+  await expect(page.locator('h6:has-text("Dashboard")')).toBeVisible();
+
+}); 
+
+
+const invalidUsername = process.env.ORANGE_HRM_INVALID_USERNAME;
+if (!invalidUsername) throw new Error('INVALID USERNAME not set');
+const invalidPassword = process.env.ORANGE_HRM_INVALID_PASSWORD;
+if (!invalidPassword) throw new Error('INVALID PASSWORD not set');
+// Negative login test for invalid username
+test('negative login test for invalid username', async ({ page }) => {
+    await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
+  // Fill in invalid username
+  await page.locator('input[name="username"]').fill('invalidUser');
+
+  // Fill in valid password
+  await page.locator('input[name="password"]').fill('password');
+
+  // Click login button
+  await page.locator('button[type="submit"]').click();
+
+  // Expect an error message to be visible
+  await expect(page.locator('.oxd-alert-content-text')).toHaveText('Invalid credentials');
+});
+
+// Negative login test for invalid password
+test('negative login test for invalid password', async ({ page }) => {
+    await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
+  // Fill in valid username
+  await page.locator('input[name="username"]').fill('username');
+
+  // Fill in invalid password
+  await page.locator('input[name="password"]').fill('invalidPassword');
+
+  // Click login button
+  await page.locator('button[type="submit"]').click();
+
+  // Expect an error message to be visible
+  await expect(page.locator('.oxd-alert-content-text')).toHaveText('Invalid credentials');
+});
